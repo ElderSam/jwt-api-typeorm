73c9201927ee12d7933b1a33a6b080db345e5f4c
ElderSam <eldersamuel98@gmail.com>
2021-03-02 16:44:08 -0300

    Criar rota de autentificação

---
+++ src/app/controllers/AuthController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/Users';
+
+class AuthController {
+    async authenticate(req: Request, res: Response) {
+        const repository = getRepository(User);
+        const { email, password } = req.body;
+
+        const user = await repository.findOne({ where: { email } });
+
+        if(!user) {
+            return res.sendStatus(401);
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if(!isValidPassword) {
+            return res.sendStatus(401);
+        }
+
+        const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: '1d' });
+        /* the second parameter of sign() is sensitive data,
+            so the ideal would be to get it through a dotenv
+            or a local file (which does not go to github) in a real application, for security reasons.
+        */
+
+        interface auxUser {
+            password?: string; // the password can be `undefined`
+        }
+
+        const auxUser: auxUser = user;
+        delete auxUser.password; // not to send the password to the frontend
+
+        return res.json({
+            auxUser,
+            token
+        });
+        
+
+    }
+}
+
+export default new AuthController();
